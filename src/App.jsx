f9bd4c08c3084939f33c89d8e49eddb9ce8a36af
Alona Chubenko <aliona.chubenko@gmail.com>
2023-12-10 08:20:58 +0200

    Add push and pop functions

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,28 @@ function App() {
   const [stack, setStack] = useState([])
   //.push() and .pop() methods
   function push() {
-
+    let newStack = [...stack];
+    let newItem = stack.length;
+    newStack.push(newItem);
+    setStack(newStack);
   }
 
   function pop() {
-
+    let newStack = [...stack];
+    newStack.pop();
+    setStack(newStack);
   }
 
   return (
     <>
       <div className="container">
-        <div className="inputValue">
-          <h1>Stack App</h1>
-          <button onClick={push}>Add</button>
-          <button onClick={pop}>Remove</button>
-          <div className={stack.length < 0 ? "red-like material-symbols-outlined" : "black-like material-symbols-outlined"}>
-
-          </div>
+        <h1>Stack App</h1>
+        <button onClick={push}>Add</button>
+        <button onClick={pop}>Remove</button>
+        <div className='inputContainer'>
+          {stack.length < 0 ? <p>It is not a stack!</p> : stack.map((element) => <span key={element} className="hearts material-symbols-outlined" > heart_plus</span>)}
         </div>
       </div>
-
-
-
     </>
   )
 }
